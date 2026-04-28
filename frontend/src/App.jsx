a02c2882de95e2b61353d2a349c83b4d4799c219
Markos Arroyo <markosarroyo@gmail.com>
2026-04-28 18:47:16 +0200

    Frontend LoginPage and RegisterLogin and protection routes fiexed in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+
+import { ProtectedRoute, PublicRoute } from './components/ProtectedRoute'
+import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 import BooksPage from './pages/BooksPage'
 import BookDetailPage from './pages/BookDetailpage'
-import Navbar from './components/Navbar'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/books" element={<BooksPage />} />
-        <Route path="/books/:id" element={<BookDetailPage />} />
-      </Routes>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+
+          {/* ── Rutas públicas: si ya estás logado redirigen a "/" ── */}
+          <Route element={<PublicRoute />}>
+            <Route path="/login"    element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+
+          {/* ── Rutas protegidas: requieren JWT válido ── */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/"          element={<HomePage />} />
+            <Route path="/books"     element={<BooksPage />} />
+            <Route path="/books/:id" element={<BookDetailPage />} />
+          </Route>
+
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
